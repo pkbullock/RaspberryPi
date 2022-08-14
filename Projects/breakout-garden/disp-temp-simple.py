68075b2d85daa2c22c8ac07d4c3abd6d8798ca0d
Paul Bullock <pkbullock@users.noreply.github.com>
2022-08-14 16:55:49 +0100

    added simplier version

---
+++ Projects/breakout-garden/disp-temp-simple.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+import time
+import colorsys
+import sys
+import ST7735
+
+from bmp280 import BMP280
+
+try: 
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+# Initialise the BMP280
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+from pms5003 import PMS5003, ReadTimeoutError as pmsReadTimeoutError, SerialTimeoutError
+from subprocess import PIPE, Popen
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from fonts.ttf import RobotoMedium as UserFont
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("""combined.py - Displays readings from all of Enviro plus' sensors
+
+Press Ctrl+C to exit!
+
+""")
+
+# BMP280 temperature/pressure/humidity sensor
+bmp280 = BMP280()
+
+# Create ST7735 LCD display class
+st7735 = ST7735.ST7735(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=12,
+    rotation=270,
+    spi_speed_hz=10000000
+)
+
+# Initialize display
+st7735.begin()
+
+WIDTH = st7735.width
+HEIGHT = st7735.height
+
+# Set up canvas and font
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+font_size_small = 10
+font_size_large = 18
+font = ImageFont.truetype(UserFont, font_size_large)
+smallfont = ImageFont.truetype(UserFont, font_size_small)
+x_offset = 2
+y_offset = 2
+
+message = ""
+
+# The position of the top bar
+top_pos = 25
+
+# Create a values dict to store the data
+variables = ["temperature",
+             "pressure",
+             "humidity"]
+
+units = ["C",
+         "hPa",
+         "%"]
+
+# Define your own warning limits
+# The limits definition follows the order of the variables array
+# Example limits explanation for temperature:
+# [4,18,28,35] means
+# [-273.15 .. 4] -> Dangerously Low
+# (4 .. 18]      -> Low
+# (18 .. 28]     -> Normal
+# (28 .. 35]     -> High
+# (35 .. MAX]    -> Dangerously High
+# DISCLAIMER: The limits provided here are just examples and come
+# with NO WARRANTY. The authors of this example code claim
+# NO RESPONSIBILITY if reliance on the following values or this
+# code in general leads to ANY DAMAGES or DEATH.
+limits = [[4, 18, 28, 35],
+          [250, 650, 1013.25, 1015],
+          [20, 30, 60, 70],
+          [-1, -1, 30000, 100000],
+          [-1, -1, 40, 50],
+          [-1, -1, 450, 550],
+          [-1, -1, 200, 300],
+          [-1, -1, 50, 100],
+          [-1, -1, 50, 100],
+          [-1, -1, 50, 100]]
+
+# RGB palette for values on the combined screen
+palette = [(0, 0, 255),           # Dangerously Low
+           (0, 255, 255),         # Low
+           (0, 255, 0),           # Normal
+           (255, 255, 0),         # High
+           (255, 0, 0)]           # Dangerously High
+
+values = {}
+
+
+# Displays data and text on the 0.96" LCD
+def display_text(variable, data, unit):
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [data]
+    # Scale the values for the variable between 0 and 1
+    vmin = min(values[variable])
+    vmax = max(values[variable])
+    colours = [(v - vmin + 1) / (vmax - vmin + 1) for v in values[variable]]
+    # Format the variable name and value
+    message = "{}: {:.1f} {}".format(variable[:4], data, unit)
+    logging.info(message)
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (255, 255, 255))
+    for i in range(len(colours)):
+        # Convert the values to colours from red to blue
+        colour = (1.0 - colours[i]) * 0.6
+        r, g, b = [int(x * 255.0) for x in colorsys.hsv_to_rgb(colour, 1.0, 1.0)]
+        # Draw a 1-pixel wide rectangle of colour
+        draw.rectangle((i, top_pos, i + 1, HEIGHT), (r, g, b))
+        # Draw a line graph in black
+        line_y = HEIGHT - (top_pos + (colours[i] * (HEIGHT - top_pos))) + top_pos
+        draw.rectangle((i, line_y, i + 1, line_y + 1), (0, 0, 0))
+    # Write the text at the top in black
+    draw.text((0, 0), message, font=font, fill=(0, 0, 0))
+    st7735.display(img)
+
+
+# Saves the data to be used in the graphs later and prints to the log
+def save_data(idx, data):
+    variable = variables[idx]
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [data]
+    unit = units[idx]
+    message = "{}: {:.1f} {}".format(variable[:4], data, unit)
+    logging.info(message)
+
+
+# Displays all the text on the 0.96" LCD
+def display_everything():
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))
+    column_count = 1
+    row_count = (len(variables) / column_count)
+    for i in range(len(variables)):
+        variable = variables[i]
+        data_value = values[variable][-1]
+        unit = units[i]
+        x = x_offset + ((WIDTH // column_count) * (i // row_count))
+        y = y_offset + ((HEIGHT / row_count) * (i % row_count))
+        message = "{}: {:.1f} {}".format(variable[:4], data_value, unit)
+        lim = limits[i]
+        rgb = palette[0]
+        for j in range(len(lim)):
+            if data_value > lim[j]:
+                rgb = palette[j + 1]
+        #draw.text((x, y), message, font=smallfont, fill=rgb)
+        draw.text((x, y), message, font=font, fill=rgb)
+    st7735.display(img)
+
+
+# Get the temperature of the CPU for compensation
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+
+def main():
+    # Tuning factor for compensation. Decrease this number to adjust the
+    # temperature down, and increase to adjust up
+    factor = 2.25
+
+    cpu_temps = [get_cpu_temperature()] * 5
+
+    delay = 0.5  # Debounce the proximity tap
+    mode = 10    # The starting mode
+    sleepFor = 2 # Sleep for x seconds
+    last_page = 0
+
+    for v in variables:
+        values[v] = [1] * WIDTH
+
+    # The main loop
+    try:
+        while True:
+
+            # Everything on one screen
+            cpu_temp = get_cpu_temperature()
+            # Smooth out with some averaging to decrease jitter
+            cpu_temps = cpu_temps[1:] + [cpu_temp]
+            avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+            raw_temp = bmp280.get_temperature()
+            raw_data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+            save_data(0, raw_data)
+            display_everything()
+            raw_data = bmp280.get_pressure()
+            save_data(1, raw_data)
+            display_everything()
+            raw_data = bmp280.get_humidity()
+            save_data(2, raw_data)
+            display_everything()
+            pms_data = None
+            time.sleep(sleepFor)
+                
+    # Exit cleanly
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
