5df139cc44c13940847af0f9badb88cefcb2d3df
Paul Bullock <paul.bullock@capacreative.co.uk>
2022-08-28 20:17:32 +0100

    setup dotnet

---
+++ Projects/dotnet/Install-PS.sh
@@ -0,0 +1,39 @@
+
+###################################
+# Prerequisites
+
+# Update package lists
+sudo apt-get update
+
+# Install libunwind8 and libssl1.0
+# Regex is used to ensure that we do not install libssl1.0-dev, as it is a variant that is not required
+sudo apt-get install '^libssl1.0.[0-9]$' libunwind8 -y
+
+###################################
+# Download and extract PowerShell
+
+# Grab the latest tar.gz
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-arm32.tar.gz
+
+# Make folder to put powershell
+mkdir ~/powershell
+
+# Unpack the tar.gz file
+tar -xvf ./powershell-7.0.3-linux-arm32.tar.gz -C ~/powershell
+
+# Start PowerShell
+
+
+
+
+# Start PowerShell from bash with sudo to create a symbolic link
+sudo ~/powershell/pwsh -c New-Item -ItemType SymbolicLink -Path "/usr/bin/pwsh" -Target "$PSHOME/pwsh" -Force
+
+# alternatively you can run following to create a symbolic link
+# sudo ln -s ~/powershell/pwsh /usr/bin/pwsh
+
+# Now to start PowerShell you can just run "pwsh"
+
+
+#Uninstallation - Raspbian
+#rm -rf ~/powershell
