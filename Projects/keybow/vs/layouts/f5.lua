cf9b84ef0e8cc4c52a53ccb4e4f5f47b5f1e2c11
Paul Bullock <paul.bullock@capacreative.co.uk>
2023-07-07 09:20:24 +0100

    Added f5

---
+++ Projects/keybow/vs/layouts/f5.lua
@@ -0,0 +1,49 @@
+require "keybow"
+
+function handle_key_00(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_01(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_02(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_03(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_04(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_05(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_06(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_07(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_08(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_09(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_10(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_11(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
