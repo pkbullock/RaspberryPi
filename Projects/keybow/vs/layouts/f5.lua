a1312b0732d2cc7e1b79f26bd2adb44443734135
Paul Bullock <paul.bullock@capacreative.co.uk>
2023-07-16 14:58:43 +0100

    updated with lights

--- Projects/keybow/vs/layouts/f5.lua
+++ Projects/keybow/vs/layouts/f5.lua
@@ -1,49 +1,104 @@
 require "keybow"
 
+function setup() -- Set custom lights up
+    keybow.auto_lights(false)
+    keybow.clear_lights()
+end
+
+lightStep = 21.25
+countSteps = 0
+
+function lightUp(buttonIndex)
+    
+    if countSteps > 255 then
+        keybow.clear_lights()
+        countSteps = 0
+    end
+
+    countSteps = countSteps + lightStep
+    keybow.set_pixel(buttonIndex, 0, 0, countSteps)
+end
+
 function handle_key_00(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(0)
+    end
 end
 
 function handle_key_01(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(1)
+    end
 end
 
 function handle_key_02(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(2)
+    end
 end
 
 function handle_key_03(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(3)
+    end
 end
 
 function handle_key_04(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(4)
+    end
 end
 
 function handle_key_05(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(5)
+    end
 end
 
 function handle_key_06(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(6)
+    end
 end
 
 function handle_key_07(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(7)
+    end
 end
 
 function handle_key_08(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(8)
+    end
 end
 
 function handle_key_09(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(9)
+    end
 end
 
 function handle_key_10(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(10)
+    end
 end
 
 function handle_key_11(pressed)
-    keybow.set_key(keybow.F5, pressed)
+    if pressed then
+        keybow.set_key(keybow.F5, pressed)
+        lightUp(11)
+    end
 end
